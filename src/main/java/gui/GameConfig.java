337831e112b4cd2920e1f60868723d9185a6c2da
Malgreen <martin@algreen.dk>
2022-04-25 22:09:30 +0200

    game config additions

--- src/main/java/gui/GameConfig.java
+++ src/main/java/gui/GameConfig.java
@@ -1,5 +1,7 @@
 package main.java.gui;
 
+import main.java.Game;
+
 import javax.swing.*;
 import java.awt.event.*;
 
@@ -7,6 +9,10 @@ public class GameConfig extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
+    private JTextField fieldModDir;
+    private JTextField fieldDeployDir;
+    private JLabel textModDir;
+    private JLabel textDeployDir;
 
     public GameConfig() {
         setContentPane(contentPane);
@@ -41,7 +47,11 @@ public class GameConfig extends JDialog {
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
 
-    private void onOK() {
+    private Game onOK() {
+        String modDeployFolder = fieldDeployDir.getText();
+        String modFolder = fieldModDir.getText();
+        String modlistfolder
+        Game game = new Game("test")
         // add your code here
         dispose();
     }
